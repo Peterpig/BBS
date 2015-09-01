7739545ea082d3714a2574ad1609f03b6d6b289f
Mr.orange <309871271@qq.com>
2015-09-01 18:15:46 +0800

    new style

--- apps/index/index.py
+++ apps/index/index.py
@@ -7,8 +7,11 @@ from django.shortcuts import render_to_response
 log = logging.getLogger(__name__)
 
 def index(request):
+    context = {}
     try:
-        context = {}
+        num = [i for i in xrange(0,20)]
+        print num
+        context['num'] = num
     except Exception, e:
         log.error("%s:%s" % (inspect.stack()[0][3], e))
 
