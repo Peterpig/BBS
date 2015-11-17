558c1dc1f08c349a5f4be5ef287526200be08fc2
Mr.orange <309871271@qq.com>
2015-11-17 18:05:37 +0800

    add 新加坡

--- settings.py
+++ settings.py
@@ -30,6 +30,17 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'bbs',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
+        'PASSWORD': 'aA4130187',                  # Not used with sqlite3.
+        'HOST': '163.44.149.163',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'bbs',                      # Or path to database file if using sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
         'PASSWORD': '123',                  # Not used with sqlite3.
         'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
