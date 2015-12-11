d4213877a9398028e5c20d10a170ff8253d2c62f
Mr.orange <309871271@qq.com>
2015-12-11 17:59:19 +0800

    wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,14 @@
-"""
-WSGI config for BBS project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
-"""
+#!/usr/bin/env python
+# coding: utf-8
 
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+# 将系统的编码设置为UTF8
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BBS.settings")
 
-application = get_wsgi_application()
+from django.core.handlers.wsgi import WSGIHandler
+application = WSGIHandler()
