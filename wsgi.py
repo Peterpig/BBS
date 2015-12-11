362aa0a1f0a2abea4d23c648587aeab2edcdeafa
Mr.orange <309871271@qq.com>
2015-12-11 18:09:17 +0800

    ss

--- wsgi.py
+++ wsgi.py
@@ -4,6 +4,10 @@
 import os
 import sys
 
+# 设置工作目录
+sys_base_path = os.path.abspath(__file__)
+sys.path.append(os.path.normpath(os.path.join(sys_base_path, '../..')))
+print "sys.path == "sys.path,
 # 将系统的编码设置为UTF8
 reload(sys)
 sys.setdefaultencoding('utf8')
