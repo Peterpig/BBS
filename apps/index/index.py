531cf647894350f9c3e76d724d378bee9d5c476e
Mr.orange <309871271@qq.com>
2016-01-16 16:30:37 +0800

    fix bug

--- apps/index/index.py
+++ apps/index/index.py
@@ -173,67 +173,70 @@ def download_file(request):
     from django.http import HttpResponse
     port = request.GET.get("port","")
     password = request.GET.get("password","")
+    s1 = request.REQUEST.get('s1', '')
+    s2 = request.REQUEST.get('s2', '')
     response = HttpResponse(mimetype='text/plain')
     file_name = u"gui-config"
                                        
     response['Content-Disposition'] = 'attachment; filename=%s.json' % file_name.encode('GBK')
     
-    response.write(u"""
-        {
-            "configs" : [
-                {
-                    "remarks" : "",
-                    "server" : "%s",
-                    "server_port" : %s,
-                    "password" : "%s",
-                    "method" : "aes-256-cfb",
-                    "obfs" : "plain",
-                    "obfsparam" : "",
-                    "remarks_base64" : "",
-                    "tcp_over_udp" : false,
-                    "udp_over_tcp" : false,
-                    "protocol" : "origin",
-                    "obfs_udp" : false,
-                    "enable" : true,
-                    "id" : "0E-25-50-5D-4F-C9-27-22-E7-1E-09-77-15-81-93-17"
-                },
-                {
-                    "remarks" : "",
-                    "server" : "%s",
-                    "server_port" : %s,
-                    "password" : "%s",
-                    "method" : "aes-256-cfb",
-                    "obfs" : "plain",
-                    "obfsparam" : "",
-                    "remarks_base64" : "",
-                    "tcp_over_udp" : false,
-                    "udp_over_tcp" : false,
-                    "protocol" : "origin",
-                    "obfs_udp" : false,
-                    "enable" : true,
-                    "id" : "0D-D0-62-25-64-AB-E1-6E-70-58-D7-6A-2A-B8-2E-B3"
-                }
-            ],
-            "index" : 0,
-            "random" : false,
-            "global" : false,
-            "enabled" : true,
-            "shareOverLan" : false,
-            "isDefault" : false,
-            "localPort" : 1080,
-            "pacUrl" : "http://127.0.0.1:1080/pac",
-            "useOnlinePac" : false,
-            "reconnectTimes" : 3,
-            "randomAlgorithm" : 0,
-            "TTL" : 0,
-            "proxyEnable" : false,
-            "proxyType" : 0,
-            "proxyHost" : null,
-            "proxyPort" : 0,
-            "proxyAuthUser" : null,
-            "proxyAuthPass" : null,
-            "autoban" : false
-        }
-    """% (s1, port, password, s2, port, password)
-    )
+    if port and password and s1 and s2:
+        response.write(u"""
+            {
+                "configs" : [
+                    {
+                        "remarks" : "",
+                        "server" : "%s",
+                        "server_port" : %s,
+                        "password" : "%s",
+                        "method" : "aes-256-cfb",
+                        "obfs" : "plain",
+                        "obfsparam" : "",
+                        "remarks_base64" : "",
+                        "tcp_over_udp" : false,
+                        "udp_over_tcp" : false,
+                        "protocol" : "origin",
+                        "obfs_udp" : false,
+                        "enable" : true,
+                        "id" : "0E-25-50-5D-4F-C9-27-22-E7-1E-09-77-15-81-93-17"
+                    },
+                    {
+                        "remarks" : "",
+                        "server" : "%s",
+                        "server_port" : %s,
+                        "password" : "%s",
+                        "method" : "aes-256-cfb",
+                        "obfs" : "plain",
+                        "obfsparam" : "",
+                        "remarks_base64" : "",
+                        "tcp_over_udp" : false,
+                        "udp_over_tcp" : false,
+                        "protocol" : "origin",
+                        "obfs_udp" : false,
+                        "enable" : true,
+                        "id" : "0D-D0-62-25-64-AB-E1-6E-70-58-D7-6A-2A-B8-2E-B3"
+                    }
+                ],
+                "index" : 0,
+                "random" : false,
+                "global" : false,
+                "enabled" : true,
+                "shareOverLan" : false,
+                "isDefault" : false,
+                "localPort" : 1080,
+                "pacUrl" : "http://127.0.0.1:1080/pac",
+                "useOnlinePac" : false,
+                "reconnectTimes" : 3,
+                "randomAlgorithm" : 0,
+                "TTL" : 0,
+                "proxyEnable" : false,
+                "proxyType" : 0,
+                "proxyHost" : null,
+                "proxyPort" : 0,
+                "proxyAuthUser" : null,
+                "proxyAuthPass" : null,
+                "autoban" : false
+            }
+        """% (s1, port, password, s2, port, password)
+        )
     return response
